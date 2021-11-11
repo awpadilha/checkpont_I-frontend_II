d1209e88d455e36a6a6adf4c0ab83923d678561a
André Padilha <awpadilha.dev@gmail.com>
2021-11-11 17:25:06 -0300

    alteracoes gerais

---
+++ README.md
@@ -0,0 +1,22 @@
+# Checkpoint 01 -  Front End II
+## Objetivo
+
+	Olá Houser, tudo bem com você? Esta atividade tem como objetivo servir como primeiro instrumento avaliativo da disciplina. Se atente às instruções e boa avaliação. O entregável será uma página em HTML5, CSS3 e Javascript seguindo os conteúdos ministrados até aqui. 
+
+    Instruções e requisitos do entregável:
+
+	O tema da aplicação é de sua preferência. Pode ser sobre carros, animes, cidades ou outro tema. A aplicação deve ser responsiva.
+    A aplicação consiste em um formulário que ao ser preenchido cria cards com as informações preenchidas.
+
+    FORMULÁRIO: 
+
+    O formulário deve ter pelo menos 4 campos: Título, Descrição e URL da imagem e um botão de submit.
+    Após o preenchimento, ao clicar no botão de submit, as informações devem ser adicionadas a um novo card na página.
+
+    CARDS:
+    Os cards devem ter uma imagem e constar as informações previamente preenchidas no formulário. O comportamento se dará de forma que as informações preenchidas sejam exibidas nesta seção de cards.
+
+    ENTREGA:
+    Em trio.
+    Devem ser entregues os arquivos HTML5, CSS3 e Javascript do projeto via Git/Github. Você deverá enviar o link do repositório por este formulário:
+    Até o dia 11/11/2021 às 23:59. 
--- js/script.js
+++ js/script.js
@@ -1,61 +1,56 @@
-// selecionar o elemento do campo 1
+//Selecionando os IDs
 let titleForm = document.querySelector('#title-form');
-// selecionar o elemento do campo 2
 let descriptionForm = document.querySelector('#description-form');
-// selecionar o elemento do campo 3
 let urlForm = document.querySelector('#url-form');
-
-// selecionar todos os radio buttons
-const radioForm = document.querySelectorAll('input[name=\'option\']')
-
-// selecionar todos os checkbox
-const checkForm = document.querySelectorAll('input[type=\'checkbox\']');
-
-// selecionar o elemento do botão
 let form = document.querySelector('#form');
 
-//atrinuir um event listener
+//Selecionando o input e checkbox
+const radioForm = document.querySelectorAll('input[name=\'option\']');
+const checkForm = document.querySelector('input[type=\'checkbox\']');
+
+//Adicionando o evento
 form.addEventListener('submit', enviar);
 
+//Validação de seleção dos inputs
 function radioSelecionado(inputs) {
     for(let i = 0; i < inputs.length; i++) {
         if (inputs[i].checked) {
             return inputs[i].value;
         }
-    }    
-}
+    }   
+} 
 
-function checkSelecionado(inputs) {
-    for(let i = 0; i < inputs.length; i++) {
-        if (inputs[i].checked) {
-            return inputs[i].value;
-        }
-    }    
-}
-
+//Função principal
 function enviar(event) {
     event.preventDefault();
 
     let radio = radioSelecionado(radioForm);
-    let check = checkSelecionado(checkForm);
+  
     let wrapper = document.querySelector('#list-wrapper')
     
+    //Criando os elementos do Card
     let title = document.createElement('h3');
     let description = document.createElement('span');
     let img = document.createElement('img');
     let selecao = document.createElement('span');
+    let check = document.createElement('span');
+
 
     title.innerHTML = titleForm.value;
     description.innerHTML = descriptionForm.value;
     img.setAttribute('src', urlForm.value);
     selecao.innerHTML = radio;
-
+    check.innerHTML = checkForm.checked ? 'Apto' : 'Não apto';
+    
+    //Criando elemento DIV e os appendChild
     let item = document.createElement('div');
 
+    //Criando os appendChild
     item.appendChild(title);
     item.appendChild(description);
     item.appendChild(img);
     item.appendChild(selecao);
+    item.appendChild(check);
     
     wrapper.appendChild(item);
 }
